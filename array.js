5677c457ea832548b0eab23e212eb78f2dec26b3
Brendan Ko <brenny0707@gmail.com>
2018-04-23 10:49:41 -0400

    add perms

---
+++ array.js
@@ -0,0 +1,16 @@
+//Permutation, find all permutations of an array(assume all elements are unique or if there are duplicates, include them again)
+function perms(arr) {
+  if (!Array.isArray(arr)) return "Not an Array";
+  if (arr.length === 0) return [];
+  if (arr.length === 1) return arr;
+  const finalPerms = [];
+  arr.forEach( (el, idx) => {
+    const first = el;
+    const rest = arr.slice(0, idx).concat(arr.slice(idx + 1));
+    const restPerms = perms(rest);
+    restPerms.forEach( (perm) => {
+      finalPerms.push([first].concat(perm));
+    });
+  });
+  return finalPerms;
+}
