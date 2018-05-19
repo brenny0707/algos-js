26f57995651e1456a09ab3b678c33707b01802b0
Brendan Ko <brenny0707@gmail.com>
2018-05-19 13:24:29 -0400

    Add process for morris traversal

--- binary_tree.js
+++ binary_tree.js
@@ -21,6 +21,20 @@ function postOrder(root) {
   console.log(root.data);
 }
 
+/* Morris Traversal
+DOES NOT USE RECURSION OR A STACK
+Create links to Inorder successor and print the data using these links, and finally revert the changes to restore original tree.
+1. Initialize current as root
+2. While current is not NULL
+   If current does not have left child
+      a) Print current’s data
+      b) Go to the right, i.e., current = current->right
+   Else
+      a) Make current as right child of the rightmost
+         node in current's left subtree
+      b) Go to this left child, i.e., current = current->left
+*/
+
 module.exports = {
   inOrder: inOrder,
   preOrder: preOrder,
