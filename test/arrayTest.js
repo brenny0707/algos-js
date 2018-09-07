2ee4cb42a3ea546c8df621e8f8d70df9b6198fd8
Brendan Ko <brenny0707@gmail.com>
2018-09-07 10:59:09 -0400

    Add unique comparator test to quickSort

--- test/arrayTest.js
+++ test/arrayTest.js
@@ -25,6 +25,9 @@ describe('Array', function() {
     it('returns permutations for larger arrays', function () {
       assert.deepEqual(perms([1,2,3]), [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]);
     })
+    it('returns permutations for larger arrays', function () {
+      assert.deepEqual(perms([1, 2, 3]), [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]);
+    })
   })
   describe('sorts', function() {
     describe('quicksort', function() {
@@ -36,6 +39,14 @@ describe('Array', function() {
       })
       it('handles an array with multiple elements', function () {
         assert.deepEqual(quickSort([3,6,2,9,10,3]), [2,3,3,6,9,10]);
+      })
+      it('handles an array with multiple elements with its own comparator function', function () {
+        const testComparator = ((a, b) => {
+          if (a < b) return 1;
+          else if (a === b) return 0;
+          else return -1;
+        });
+        assert.deepEqual(quickSort([3,6,2,9,10,3], testComparator), [10,9,6,3,3,2]);
       })
     })
   })
