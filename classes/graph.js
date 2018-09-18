280b5874f1dbc514b248d26bb9bf5778993e0840
Brendan Ko <brenny0707@gmail.com>
2018-09-18 13:26:07 -0400

    Create addEdge function

--- classes/graph.js
+++ classes/graph.js
@@ -24,11 +24,26 @@ class Graph {
     this.verticies[vRef] = new Vertex(vRef);
   }
 
-  addEdge(v, w) {
-    
+  addEdge(vRef, wRef, weight) {
+    if (!this.verticies[vRef] || !this.verticies[wRef]) return "Missing vertex/verticies";
+    const adj = this.adjList;
+    const vertexRefPair = [vRef, wRef];
+    vertexRefPair.forEach( (ref, idx) => {
+      if (!adj[ref]) adj[ref] = {}; //initialize ref in AdjList if no ref was created
+      const otherVertexRef = idx === 0 ?
+                          vertexRefPair[1] :
+                          vertexRefPair[0];
+      adj[ref][otherVertexRef] = weight;
+    })
+    return this.adjList;
   }
+
   // printGraph()
 
   // bfs(v)
   // dfs(v)
 }
+const testGraph = new Graph();
+testGraph.addVertex("A");
+testGraph.addVertex("B");
+console.log(testGraph.addEdge("A", "B", 50));
