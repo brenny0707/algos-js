849b79187b3a5f28af01943c3063a6bad2ad1dda
Brendan Ko <brenny0707@gmail.com>
2018-09-18 13:09:20 -0400

    Add vertex refs and addVertex

--- classes/graph.js
+++ classes/graph.js
@@ -1,6 +1,34 @@
-class Graph {
-  constructor(numVerticies) {
-    this.numVerticies = numVerticies;
-    this.adjList = new Map();
+class Vertex {
+  constructor(ref) {
+    this.ref = ref;
   }
+}
+
+class Graph {
+  constructor() {
+    this.verticies = {};
+    this.adjList = {};
+  }
+
+  /*
+  verticies {
+    <vRef> : vertex
+  }
+
+  vertex {
+    ref: <vRef>,
+  }
+  */
+
+  addVertex(vRef) {
+    this.verticies[vRef] = new Vertex(vRef);
+  }
+
+  addEdge(v, w) {
+    
+  }
+  // printGraph()
+
+  // bfs(v)
+  // dfs(v)
 }
