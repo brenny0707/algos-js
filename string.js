94e9d488f07f2269bd8c8e7b33ac5766f7d92822
Brendan Ko <brenny0707@gmail.com>
2018-09-24 11:36:37 -0400

    Add oneAway string function

--- string.js
+++ string.js
@@ -13,3 +13,48 @@ function isUnique(str) {
    * Space complexity: 0(c) as well, since isSeen only can increase as large as c before returning false on next iteration
    */
 }
+
+function oneAway(str1, str2) {
+  //checks if two strings can be equal if you can insert, delete, or replace 1 character in one of the two strings
+  if (str1 === str2) return true;
+  let differenceCounter = 0;
+  if (str1.length === str2.length) {
+    for (let i = 0; i < str1.length; i++) {
+      const ch1 = str1.charAt(i);
+      const ch2 = str2.charAt(i);
+      if (ch1 !== ch2) {
+        if (differenceCounter > 0) return false;
+        else differenceCounter++;
+      }
+    }
+  }
+  else {
+    if (Math.abs(str1.length - str2.length) > 1) return false;
+    else {
+      const short = str1.length < str2.length ?
+        str1 :
+        str2;
+      const long = str1.length < str2.length ?
+        str2 :
+        str1;
+      let longChOffset = 0;
+      for (i = 0; i < short.length; i++) {
+        const shortCh = short.charAt(i);
+        const longCh = long.charAt(i + longChOffset);
+        if (shortCh !== longCh && differenceCounter > 0) return false;
+        else if (shortCh !== longCh) {
+          differenceCounter++;
+          longChOffset++;
+          const nextLongCh = long.charAt(i + longChOffset);
+          if (nextLongCh !== shortCh) return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+/**
+ * Time complexity: Worst case is 0(n) where n is the length of the short string
+ * Space complexity: 0(1) due to no dynamic data structure creation
+ */
