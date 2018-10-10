7724a67a68b9f3093857f7dd393e8a619041e2c7
Brendan Ko <brenny0707@gmail.com>
2018-10-10 14:03:47 -0400

    Implement Dijkstra's algorithim for implementation of current undirected graph

--- classes/graph.js
+++ classes/graph.js
@@ -50,19 +50,67 @@ class Graph {
 
   // bfs(v)
   // dfs(v)
+
+  findShortestPath(startNodeRef, endNodeRef) {
+    const nodeQueue = [startNodeRef];
+    const nodeWeights = {};
+    nodeWeights[startNodeRef] = 0;
+    while (nodeQueue.length > 0) {
+      // console.log(nodeQueue);
+      let curNodeRef = nodeQueue.shift();
+      if (curNodeRef === endNodeRef);
+      else {
+        let curWeight = nodeWeights[curNodeRef] ? nodeWeights[curNodeRef] : 0;
+        const curNodeAdjs = this.adjList[curNodeRef]; //this.adjList["A"] => { B: 10, C: 50, D: 45 }
+        const curNodeAdjRefs = Object.keys(curNodeAdjs);
+        curNodeAdjRefs.forEach( (adjRef) => {
+          const nodeWeight = nodeWeights[curNodeRef] + curNodeAdjs[adjRef]; //weight of curNode + edge to adjNode
+          if (adjRef === startNodeRef);
+          else if (!nodeWeights[adjRef] || nodeWeight < nodeWeights[adjRef]) {
+            nodeWeights[adjRef] = nodeWeight;
+            nodeQueue.push(adjRef);
+          }
+        })
+      }
+    }
+    console.log(nodeWeights);
+    return nodeWeights[endNodeRef];
+  }
 }
-const testGraph = new Graph();
-testGraph.addVertex("A");
-testGraph.addVertex("B");
-testGraph.addVertex("C");
-testGraph.addVertex("D");
-testGraph.addEdge("A", "B", 10);
-testGraph.addEdge("A", "C", 50);
-testGraph.addEdge("A", "D", 45);
-testGraph.addEdge("B", "C", 25);
-testGraph.addEdge("B", "D", 25);
-testGraph.addEdge("C", "D", 40);
-testGraph.addEdge("C", "H", 40);
-testGraph.addEdge("W", "H", 40);
-console.log(testGraph.verticies);
-console.log(testGraph.adjList);
+// const testGraph = new Graph();
+// testGraph.addVertex("A");
+// testGraph.addVertex("B");
+// testGraph.addVertex("C");
+// testGraph.addVertex("D");
+// testGraph.addEdge("A", "B", 10);
+// testGraph.addEdge("A", "C", 50);
+// testGraph.addEdge("A", "D", 45);
+// testGraph.addEdge("B", "C", 25);
+// testGraph.addEdge("B", "D", 25);
+// testGraph.addEdge("C", "D", 40);
+// testGraph.addEdge("C", "H", 40);
+// testGraph.addEdge("W", "H", 40);
+// console.log(testGraph.verticies);
+// console.log(testGraph.adjList);
+
+const dijkstraGraph = new Graph();
+dijkstraGraph.addVertex("A");
+dijkstraGraph.addVertex("B");
+dijkstraGraph.addVertex("C");
+dijkstraGraph.addVertex("D");
+dijkstraGraph.addVertex("E");
+dijkstraGraph.addVertex("F");
+dijkstraGraph.addVertex("G");
+dijkstraGraph.addVertex("H");
+dijkstraGraph.addEdge("A", "B", 5);
+dijkstraGraph.addEdge("A", "G", 25);
+dijkstraGraph.addEdge("A", "F", 30);
+dijkstraGraph.addEdge("G", "H", 25);
+dijkstraGraph.addEdge("B", "C", 5);
+dijkstraGraph.addEdge("C", "D", 5);
+dijkstraGraph.addEdge("F", "D", 30);
+dijkstraGraph.addEdge("F", "E", 80);
+dijkstraGraph.addEdge("H", "D", 25);
+dijkstraGraph.addEdge("H", "E", 30);
+dijkstraGraph.addEdge("D", "E", 100);
+console.log(dijkstraGraph.findShortestPath("A","E"));
